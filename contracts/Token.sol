fa26c694f20223536d6d0279af05c654341b51c5
Krittawat Wisedsuwon <krittawat.wis@lightnet.io>
2021-04-01 11:57:09 +0700

    add console.log inside .sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >0.7.0;
 
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -39,6 +40,8 @@ contract Token {
      * the contract.
      */
     function transfer(address to, uint256 amount) external {
+        console.log("Sender balance is %s tokens", balances[msg.sender]);
+        console.log("Trying to send %s tokens to %s", amount, to);
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
